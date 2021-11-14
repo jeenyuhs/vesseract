d8592f038ee951345c35e8f4a6e40ddcf576a906
Benoit Malhomme <benoit.malhomme@epita.fr>
2021-11-14 12:22:31 +0100

    wrapper: Use v fmt

--- vesseract.v
+++ vesseract.v
@@ -3,20 +3,20 @@ module vesseract
 import os
 
 const (
-	supported = ["PNG", "JPEG"]
+	supported = ['PNG', 'JPEG']
 )
 
 pub struct Tesseract {
 pub:
-	image 	string
-	conf	string
-	lang 	string
-	output	string = "txt"
-} 
+	image  string
+	conf   string
+	lang   string
+	output string = 'txt'
+}
 
 fn run_and_get_output(t Tesseract) ?os.Result {
 	if !os.exists(t.image) {
-		return error("vesseract: Image not found.")
+		return error('vesseract: Image not found.')
 	}
 
 	mut args := []string{}
@@ -25,34 +25,30 @@ fn run_and_get_output(t Tesseract) ?os.Result {
 	args << t.output
 
 	if t.lang.len > 0 {
-		args << "-l "+t.lang
+		args << '-l ' + t.lang
 	}
 
 	if t.conf.len > 0 {
 		args << t.conf
 	}
-	mut s := os.execute("tesseract "+args.join(" "))
-	
+	mut s := os.execute('tesseract ' + args.join(' '))
+
 	if s.exit_code != 0 {
 		return error("vesseract - error $s.exit_code \"$s.output\"")
 	}
 
-	unsafe {s.free()}
+	unsafe { s.free() }
 
 	return s
 }
 
 pub fn extract_string(t Tesseract) ?string {
-	run_and_get_output(t) or {
-		return err
-	}
-	path := t.output+".txt"
-	
-	str := os.read_file(path) or {
-		return err
-	}
+	run_and_get_output(t) or { return err }
+	path := t.output + '.txt'
+
+	str := os.read_file(path) or { return err }
 
 	os.rm(path) ?
 
-	return str[..str.len-2]
+	return str[..str.len - 2]
 }
--- vesseract_test.v
+++ vesseract_test.v
@@ -1,11 +1,11 @@
 module vesseract
 
 fn test_string_extraction() {
-	text := extract_string(image: "sample/snippet_dan.png", lang: "dan", conf: "--oem 1 --psm 3") or { 
+	text := extract_string(image: 'sample/snippet_dan.png', lang: 'dan', conf: '--oem 1 --psm 3') or {
 		panic(err)
 	}
-	line := text.split("\n").filter(it != "")
+	line := text.split('\n').filter(it != '')
 
-	assert line[1] == "Jeg sad i havestuen i Georgia Pines med min fars fyldepen"
-	assert line[4].contains("Den Grønne Mil")
+	assert line[1] == 'Jeg sad i havestuen i Georgia Pines med min fars fyldepen'
+	assert line[4].contains('Den Grønne Mil')
 }
